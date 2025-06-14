967b94772da96af47e233b510a860399d0251586
nekus8814 <ovira@inbox.lv>
2025-06-14 10:10:49 +0300

    bucket

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "this" {
   ami                     = "ami-0becc523130ac9d5d"
   instance_type           = "t3.micro"
 }
+
+resource "aws_s3_bucket" "vedro" {
+  bucket = "vedro123a"
+}
